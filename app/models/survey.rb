af4604e0226e3361e8cb12201ec5781cb4429d67
Alain M. Lafon <alain.lafon@dispatched.ch>
2017-02-26 16:37:15 +0100

    Validation for 'sex' and styling admin dashboard

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,5 @@ class Survey < ActiveRecord::Base
   validates :rating, numericality: {greater_than: 0,
                                     less_than: 11 }
 
+  validates :sex, inclusion: {in: %w(male female)}
 end
